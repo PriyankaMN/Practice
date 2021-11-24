e1049c90130564b6386f8ac60752b57e15108482
priyankamn <priyankamn0705@gmail.com>
2021-11-24 22:42:10 +0530

    commit

---
+++ 14_armstrong.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+//input 371
+//3^3 + 7^3 + 1^3 = 371 -> amstrong no.
+
+int amstro(int n)
+{
+    int rem, res = 0;
+    while (n > 0)
+    {
+        rem = n % 10;
+        n = n / 10;
+        res = res + rem * rem * rem;
+    }
+    return res;
+}
+
+int printAmstroRange(int n)
+{
+    int rem, res = 0;
+    while (n > 0)
+    {
+        rem = n % 10;
+        n = n / 10;
+        res = res + rem * rem * rem;
+    }
+    return res;
+}
+
+int main()
+{
+    int num, ch;
+    cout << "1.series 2.one no." << endl;
+    cin >> ch;
+
+    if (ch == 2)
+    {
+        cout << "Enter a number" << endl;
+        cin >> num;
+        if (amstro(num) == num)
+        {
+            cout << "amstom!!";
+        }
+        else
+        {
+            cout << "not an amstorm";
+        }
+    }
+
+    else if (ch == 1)
+    {
+        cout << "Enter a number" << endl;
+        cin >> num;
+        for (int i = 0; i <= num; i++)
+        {
+            if (printAmstroRange(i) == i)
+            {
+                cout << i << " ";
+            }
+        }
+    }
+    else
+    {
+        cout << "choose either 1 or 2." << endl;
+    }
+    return 0;
+}
